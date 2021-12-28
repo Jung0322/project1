4dd7b220af21b5129495327902f48adae1053ad3
jung <3dnr22@gmail.com>
2021-12-28 18:17:16 +0900

    이미지파일 저장

---
+++ src/main/java/com/company/domain/AttachProductDTO.java
@@ -0,0 +1,16 @@
+package com.company.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class AttachProductDTO {
+	private String puuid;
+	private int pno;
+	private String puploadPath;
+	private String pimgname;
+	
+}
