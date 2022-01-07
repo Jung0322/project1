1f6dc0a6a5a750a3fb434cd397c095a025698727
myKim112 <minyk112@gmail.com>
2022-01-07 16:08:30 +0900

    로그아웃 시 이동하는 경로 변경

--- src/main/java/com/company/handler/CustomLogoutSuccessHandler.java
+++ src/main/java/com/company/handler/CustomLogoutSuccessHandler.java
@@ -22,6 +22,6 @@ public class CustomLogoutSuccessHandler implements LogoutSuccessHandler {
 			}
 		}
 		response.setStatus(HttpServletResponse.SC_OK);
-		response.sendRedirect("/product/Secindex");
+		response.sendRedirect("/product/index");
 	}
 }
