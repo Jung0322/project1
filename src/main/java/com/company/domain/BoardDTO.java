fb6a354c0d57b7f3417735a098d953b9215ee2d1
ahreum <dnflsms357@gmail.com>
2022-01-17 12:27:39 +0900

    카테고리 별 보여주기

--- src/main/java/com/company/domain/BoardDTO.java
+++ src/main/java/com/company/domain/BoardDTO.java
@@ -20,7 +20,6 @@ public class BoardDTO {
 	private String content; //글내용
 	private Date regdate; // 작성날짜
 	private Date updatedate; // 수정날짜
-	private int mcount; // 조회수
 	private int replycnt; // 댓글수
 	private int curious; // 궁금해요/공감해요 갯수
 	
