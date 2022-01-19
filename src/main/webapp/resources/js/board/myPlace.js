9859bc2d21f6690144d6163fe2004db1b74b35d6
ahreum <dnflsms357@gmail.com>
2022-01-19 17:03:45 +0900

    댓글

--- src/main/webapp/resources/js/board/myPlace.js
+++ src/main/webapp/resources/js/board/myPlace.js
@@ -231,10 +231,11 @@ $(function() {
 				str += "</div></div></div>";
 				
 				str += "<div class ='replyAreaHidden"+data[i].mrno+"' id='replyAreaHidden' style='display:none;'>";
+				str += "<div clearfix>";
 				str += "<div class='media-object pull-left' id='profile'>";
 				str += "<img src='/resources/images/temp-profile.png' class='img-responsive img-circle pro hiddenImg' alt='Blog Image'>";
 				str += "</div>";
-				str += "<div class='media-body clearfix'>";
+				str += "<div class='media-body'>";
 				str += "<span name='nickname' id='nicknameHidden' style='margin-bottom: 17px;'>" + data[i].nickname + "</span>";
 				str += "<span name='mytown' id='mytownHidden'>" + data[i].mytown + "</span></div>";
 				str += "<div><textarea name='content' id='modifyContent'></textarea></div>";
